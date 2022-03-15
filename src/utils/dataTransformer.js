01295bb9d6793e3bc7527d79b1baabc52337affd
WuJiang <wujiang5521@qq.com>
2022-03-15 16:39:25 +0800

    update

--- src/utils/dataTransformer.js
+++ src/utils/dataTransformer.js
@@ -1,6 +1,7 @@
 export function tacticTransformer(t, tId) {
     return {
         ...t,
+        tactic_surrounding: t.tactic_surrounding.slice(1, t.tactic_surrounding.length - 1),
         fixId: tId,
         fix: false,
     }
