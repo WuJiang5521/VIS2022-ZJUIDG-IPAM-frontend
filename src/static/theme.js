a383bf7d7d81f9185e035438d4d07cfe6510a07a
WuJiang <wujiang5521@qq.com>
2022-03-16 13:40:02 +0800

    update color

--- src/static/theme.js
+++ src/static/theme.js
@@ -4,7 +4,7 @@ const scrollbarColor = 'rgb(36, 40, 41)'
 export const scrollbarSize = 4;
 
 // playerColors must be in rgb form!!!
-export const playerColors = ['rgb(214,187,120)', 'rgb(78,102,128)'];
+export const playerColors = ['rgb(244, 122, 69)', 'rgb(78,102,128)'];
 export const winColors = ['rgb(101,210,114)', 'rgb(221,96,96)'];
 export const transition = (...keys) => keys.map(key => `${key} .3s cubic-bezier(0.4, 0, 0.2, 1) 0ms`).join(',');
 
